b29a95ebc088481d2cad2b588a4ae32c5e8d73b7
Osvaldo Margato <15236942+margato@users.noreply.github.com>
2020-07-24 16:55:21 -0300

    add missing folder

--- src/main/java/br/unesp/banco/core/db/operation/Entity.java
+++
@@ -1,7 +0,0 @@
-package br.unesp.banco.core.db.operation;
-
-public interface Entity<K> {
-    static String getTableName() {
-        return null;
-    }
-}
--- src/main/java/br/unesp/banco/core/db/operation/QueryMapper.java
+++
@@ -1,9 +0,0 @@
-package br.unesp.banco.core.db.operation;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface QueryMapper<T> {
-    T toEntity(ResultSet rs) throws SQLException;
-    String toQuery(T t);
-}
