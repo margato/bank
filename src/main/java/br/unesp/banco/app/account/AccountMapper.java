1f34ded9e8d7dcfd394fae161e472247f9bd07e7
gabrielcalixto <gabrielcalixto9@gmail.com>
2020-08-13 20:31:29 -0300

    remove balance attribute

--- src/main/java/br/unesp/banco/app/account/AccountMapper.java
+++ src/main/java/br/unesp/banco/app/account/AccountMapper.java
@@ -16,18 +16,16 @@ public class AccountMapper implements QueryMapper<Account> {
         account.setId(rs.getLong("id"));
         account.setNumber(rs.getString("number"));
         account.setPassword("protected");
-        account.setBalance(new Money(rs.getDouble("balance"), Currency.REAL));
 
         return account;
     }
 
     @Override
     public String toQuery(Account account) {
-        return String.format("INSERT INTO %s (number, password, balance) VALUES ('%s', '%s', %s)",
+        return String.format("INSERT INTO %s (number, password) VALUES ('%s', '%s')",
                 Account.class.getAnnotation(Entity.class).table(),
                 account.getNumber(),
-                account.getPassword(),
-                account.getBalance().getAmount().toString()
+                account.getPassword()
         );
     }
 }
