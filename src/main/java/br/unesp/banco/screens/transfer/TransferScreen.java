b65743a619b8f4b7c1d57f9f330b465ce93e1683
gabrielcalixto <gabrielcalixto9@gmail.com>
2020-08-14 23:14:44 -0300

    TransferScreen done

---
+++ src/main/java/br/unesp/banco/screens/transfer/TransferScreen.java
@@ -0,0 +1,14 @@
+package br.unesp.banco.screens.transfer;
+
+import javax.swing.*;
+
+public class TransferScreen {
+    private JPanel bodyPanel;
+    private JPanel painelBts;
+    private JButton backButton;
+    private JLabel valueLabel;
+    private JButton confirmButton;
+    private JTextField valueInput;
+    private JLabel errorMessage;
+    private JLabel accountLabel;
+}
